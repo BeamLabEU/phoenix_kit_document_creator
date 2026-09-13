2cc9794e08c4325b102540fadd4232047125c52e
Dmitri Don <fotkin@gmail.com>
2026-09-13 18:27:55 +0000

    Bump version to 0.9.3

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PhoenixKitDocumentCreator.MixProject do
   use Mix.Project
 
-  @version "0.9.2"
+  @version "0.9.3"
   @source_url "https://github.com/BeamLabEU/phoenix_kit_document_creator"
 
   def project do
